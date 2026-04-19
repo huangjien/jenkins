695efc1dc03f361aefe31d03e1cefb2c7cee444e
huangjien <huangjien@gmail.com>
2026-04-19 10:27:24 +0100

    ci: switch devops test job to pipeline from scm with gh credential

--- jobs/templates/pipeline_job_devops_test.groovy
+++ jobs/templates/pipeline_job_devops_test.groovy
@@ -2,9 +2,18 @@ pipelineJob('DevOps-Test-Docker-General-Agent') {
   description('Repo-managed pipeline job for docker agent smoke checks.')
 
   definition {
-    cps {
-      script(readFileFromWorkspace('pipelines/apps/devops-test-docker-general-agent.Jenkinsfile'))
-      sandbox()
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/huangjien/jenkins.git')
+            credentials('gh_token')
+          }
+          branch('*/main')
+        }
+      }
+      scriptPath('pipelines/apps/devops-test-docker-general-agent.Jenkinsfile')
+      lightweight(true)
     }
   }
 }
